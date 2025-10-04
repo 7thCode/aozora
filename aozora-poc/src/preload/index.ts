106015baf9373e98307f696bb6c056ab25e23350
MIKIO ODA <oda.mikio@gmail.com>
2025-10-04 23:09:00 +0900

    クリーニング

--- aozora-poc/src/preload/index.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  downloadNovel: (url: string) => ipcRenderer.invoke('download-novel', url),
-  onDownloadProgress: (callback: (data: any) => void) => {
-    ipcRenderer.on('download-progress', (_event, data) => callback(data));
-  }
-});
